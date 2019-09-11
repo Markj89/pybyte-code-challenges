affc3013535cad8f799155d4e3df8a164115ac8a
Markj89 <marcusj98@gmail.com>
2019-09-10 21:38:06 -0500

    Output Vowel Count

--- 106/exercise_6.py
+++ 106/exercise_6.py
@@ -21,13 +21,26 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """
-vowels = 'aeiou'
+vowels = 'aeiou'    
 
-def strip_vowels(text=text):
+def number_of_vowels_found(updated_text):
+    
+    # Initializing count variable to 0
+    count = 0
+    
+    for updated_word in updated_text:
+        if '*' in updated_word:                
+                
+            # Lets count it up!
+            count = count + 1
+                              
+    return count
+
+
+def replaced_text(text=text):
     
     # vars
     zen_list = []
-    count = 0
     
     # Convert string to list
     text = text.split("\n")
@@ -40,25 +53,21 @@ def strip_vowels(text=text):
             
             # If vowel is present
             if letter in vowels:
+                    
+                # Pass the value
                 letter = letter.replace(letter, '*')
-            
+
             # Append to new string    
             zen_list.append(letter)
-            
-    str = ''.join(zen_list) 
+    
+        number_of_vowels_found(zen_list)    
+    str = ''.join(zen_list)
     return str
     
-print(strip_vowels())
-    
-def count_vowels(updated_text):
-    
-    # Initializing count variable to 0
-    count = 0
-    while updated_text:
-        count = count + 1
-                              
-    return count
+#print(replaced_text())
+output = number_of_vowels_found(replaced_text())
 
+print(output)
 
 ##def strip_vowels(text: str) -> (str, int):
     ##"""Replace all vowels in the input text string by a star
